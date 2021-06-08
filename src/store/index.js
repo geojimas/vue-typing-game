bd7d9b083a4a52f92d81d106927ac453ab873a9a
Dimitris Georgiadis <geojimas@gmail.com>
2021-06-08 21:52:06 +0300

    version 1.3

--- src/store/index.js
+++ src/store/index.js
@@ -2,33 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    keywords: [
-      'platform',
-      'superior',
-      'gradual'
-      // 'established',
-      // 'radiation',
-      // 'housing',
-      // 'consideration',
-      // 'entitlement',
-      // 'absence',
-      // 'association',
-      // 'countryside',
-      // 'episode',
-      // 'prejudice',
-      // 'sympathetic',
-      // 'privilege',
-      // 'interference',
-      // 'marketing',
-      // 'paragraph',
-      // 'advertising',
-      // 'snarl'
-    ]
   },
   getters: {
-    getKeywords (state) {
-      return state.keywords
-    }
   },
   mutations: {
   },
